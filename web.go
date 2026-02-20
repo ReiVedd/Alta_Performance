a16373d542c46ae17c018d664c33076dffcfc959
ReiVedd <rsilvadiogenes@gmail.com>
2026-02-19 23:06:47 -0300

    first commit

---
+++ web.go
@@ -0,0 +1,16 @@
+package main
+import "fmt"
+
+/*
+1. ESCALABILIDADE: Se o site estava ativo e funcionando normalmente,e após o sistema estar sobre carga gerou lentidão, significa que ao escalar de maneeira rápida o site não conseguiu supotar.	
+
+2. CONFIABILIDADE: Se o meu sistema após estar em alta demanda vaza dados de um cliente eu tenho um problema de confiança, o sistema precisa ser seguro independente de qual cenário seja apresentado a ele.
+
+3. MANUNTENIBILIDADE: Caso o sistema não tiver um padrão definido de códigos, torna a manutenção complexa e demorada, resultando em uma necessidade de parar o sistema para poder modificar ele, que ocasiona perca de dinheiro.
+*/
+
+func main() {
+	fmt.Println("Ambiente configurado por: Diogenes")
+
+	fmt.Scanln()
+}
